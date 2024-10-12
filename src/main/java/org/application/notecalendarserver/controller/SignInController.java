c9d497e42314e899d9a8a7b7e1a5d3e0ffca8def
minmise <dokhru@gmail.com>
2024-10-12 12:06:16 +0300

    add registration/authorization requests processing

--- src/main/java/org/application/notecalendarserver/controller/SignInController.java
+++ src/main/java/org/application/notecalendarserver/controller/SignInController.java
@@ -4,17 +4,16 @@ import org.application.notecalendarserver.model.User;
 import org.application.notecalendarserver.repository.UserRepository;
 import org.application.notecalendarserver.service.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/signIn")
+@RestController
+@RequestMapping("/signIn")
 public class SignInController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping()
+    @PostMapping
     public Long signIn(@RequestBody User user) {
         return AuthorizationService.getSignIn(user, userRepository);
     }
--- src/main/java/org/application/notecalendarserver/controller/SignUpController.java
+++ src/main/java/org/application/notecalendarserver/controller/SignUpController.java
@@ -5,19 +5,17 @@ import org.application.notecalendarserver.repository.UserRepository;
 import org.application.notecalendarserver.service.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/signUp")
+@RestController
+@RequestMapping("/signUp")
 public class SignUpController {
 
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping
-    Long signUp(@RequestBody User user) {
+    public Long signUp(@RequestBody User user) {
         return AuthorizationService.getSignUp(user, userRepository);
     }
 
