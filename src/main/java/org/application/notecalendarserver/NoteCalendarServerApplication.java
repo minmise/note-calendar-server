d96e9ce51daff0f6ddedf46cebf505b320b9d28a
minmise <dokhru@gmail.com>
2024-10-12 07:01:28 +0300

    Initial commit

---
+++ src/main/java/org/application/notecalendarserver/NoteCalendarServerApplication.java
@@ -0,0 +1,13 @@
+package org.application.notecalendarserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteCalendarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteCalendarServerApplication.class, args);
+	}
+
+}
